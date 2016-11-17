371de24523dc966efdb08ff3664824c0f6b32992
Binh Khuu <binhkhuu90@gmail.com>
2016-11-17 14:00:02 +0800

    finished bonus exercise for 02_calculator

--- 02_calculator/calculator.rb
+++ 02_calculator/calculator.rb
@@ -16,3 +16,38 @@ def sum numbers
 	end
 	total
 end
+
+def multiply (num_1, num_2)
+	num_1 * num_2
+end
+
+def multiplies numbers
+	total = 0
+	if numbers.length > 0
+		total = 1
+		for num in numbers
+			total *= num
+		end
+	end
+	total
+end
+
+def power (num,exp)
+	total = 1
+	if exp == 0
+		total = 1
+	else 
+		for ii in 0...exp
+			total = total * num
+		end
+	end
+	total
+end
+
+def factorial num
+	total = 1
+	for ii in 1...num+1
+		total *= ii
+	end
+	total
+end
